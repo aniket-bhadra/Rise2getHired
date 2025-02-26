19731ca6537d8ce9a87fdc8f94ec131b7d63a8d8
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-02-26 11:44:38 +0530

    login funcitonlity completed

--- client/components/authComponent/login.jsx
+++ client/components/authComponent/login.jsx
@@ -2,13 +2,36 @@ import React, { useState } from "react";
 import { View, TextInput, TouchableOpacity, Text } from "react-native";
 import styles from "./styles";
 import { useRouter } from "expo-router";
+import { useContext } from "react";
+import { apiBaseUrl } from "../../config/config";
+import { TimerContext } from "../../context/TimerContext";
+
+import axios from "axios";
+import { Alert } from "react-native";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const handleLogin = () => {
-    router.push("/jobs/jobs-home");
+  const { setUser } = useContext(TimerContext);
+  const handleLogin = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`${apiBaseUrl}/api/user/login`, {
+        email,
+        password,
+      });
+      setUser(response.data);
+      router.push("/jobs/jobs-home");
+    } catch (error) {
+      Alert.alert(
+        "Login Failed",
+        error.response?.data?.message || error.message
+      );
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
