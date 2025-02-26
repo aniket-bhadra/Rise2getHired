6728fb29538e0064993929040c2c49a4b7315940
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-02-26 13:59:04 +0530

    all the routes to update jobs created

---
+++ server/Routes/userUpdateRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  saveJob,
+  saveAffirmation,
+  updateLastBrowsedJob,
+} from "../Controllers/userController.js";
+
+const router = express.Router();
+
+router.post("/save-job", saveJob);
+router.post("/save-affirmation", saveAffirmation);
+router.post("/update-last-browsed", updateLastBrowsedJob);
+
+export default router;
